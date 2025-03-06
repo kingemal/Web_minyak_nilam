9a69cd30338e9eff4c66eff5ff6a4ef31e636614
Kemalfarsa007 <113975189+kingemal@users.noreply.github.com>
2025-03-06 21:59:49 +0700

    Update webnilam.py

--- webnilam.py
+++ webnilam.py
@@ -510,6 +510,3 @@ def main():
 if __name__ == '__main__':
     main()
 st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">', unsafe_allow_html=True)
-
-
-
