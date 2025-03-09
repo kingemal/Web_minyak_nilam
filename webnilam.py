2fcd7d9ab98266b2299b80643e09cf9c11969911
Kemalfarsa007 <113975189+kingemal@users.noreply.github.com>
2025-03-10 01:05:54 +0700

    Update webnilam.py

--- webnilam.py
+++ webnilam.py
@@ -539,7 +539,7 @@ def login():
         if user:
             st.session_state.loggin = True
             st.session_state.role = role.lower()  # store role in lowercase to match with menu conditions
-            params = st.get_query_params()
+            params = st.get_query_params.to_dict()
             page = params.get("page", ["home"])[0] # Refresh the page to show the correct menu
         if page == "home":
             st.write("Home")
@@ -556,7 +556,7 @@ def main():
     st.sidebar.title("Navigasi")
     
     # Periksa query params ketika aplikasi dijalankan
-    query_params = st.get_query_params()
+    query_params = st.get_query_params.to_dict()
     Id_Penyulingan = query_params.get('Id_Penyulingan',[None][0])
 
     # Jika Id_Produksi diberikan di URL, langsung ke halaman Penelusuran
