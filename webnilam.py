ca00607db9aa939aea8b0699da018d7052a7e01b
Kemalfarsa007 <113975189+kingemal@users.noreply.github.com>
2025-03-18 23:58:23 +0700

    Update webnilam.py

--- webnilam.py
+++ webnilam.py
@@ -410,16 +410,16 @@ def penelusuran(Id_Penyulingan=None):
             if table_minyak_nilam_to_view:
                 product_info_detail = get_product_from_db(table_minyak_nilam_to_view)
                 if product_info_detail:
-                    st.write(f"**Nama Petani atau Penyuling:** {product_info_detail[6]}")
+                    st.write(f"**Nama Petani atau Penyuling:** {product_info_detail[1]}")
                     st.write(f"**Jenis Penyulingan:** {product_info_detail[2]}")
                     st.write(f"**Jumlah_Minyak:** {product_info_detail[3]}")
                     st.write(f"*Lokasi:** {product_info_detail[4]}")
-                    st.write(f"**Nama Pengepul 1 atau 2:** {product_info_detail[7]}")
-                    st.write(f"*Tanggal Penjualan ke pengepul 1 atau 2:** {product_info_detail[9]}")
+                    st.write(f"**Nama Pengepul 1 atau 2:** {product_info_detail[5]}")
+                    st.write(f"*Tanggal Penjualan ke pengepul 1 atau 2:** {product_info_detail[6]}")
 
                     # Menampilkan gambar jika ada
                     if product_info_detail[4]:
-                        image_db = Image.open(io.BytesIO(product_info_detail[4]))
+                        image_db = Image.open(io.BytesIO(product_info_detail[8]))
                         st.image(image_db, caption='Gambar dari Database SQLite', use_column_width=True)
                     else:
                         st.info("Tidak ada gambar untuk produk ini.")
