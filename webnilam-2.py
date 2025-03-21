3dbb2c1017d972eb1188dcbf64f5e901033545b7
Kemalfarsa007 <113975189+kingemal@users.noreply.github.com>
2025-03-22 01:10:21 +0700

    Update webnilam-2.py

--- webnilam-2.py
+++ webnilam-2.py
@@ -412,9 +412,9 @@ def penelusuran(Id_Penyulingan=None):
                     st.write(f"**Nama Petani atau Penyuling:** {product_info_detail[1]}")
                     st.write(f"**Jenis Penyulingan:** {product_info_detail[2]}")
                     st.write(f"**Jumlah_Minyak:** {product_info_detail[3]}")
-                    st.write(f"*Lokasi:** {product_info_detail[4]}")
-                    st.write(f"**Nama Pengepul 1 atau 2:** {product_info_detail[5]}")
-                    st.write(f"*Tanggal Penjualan ke pengepul 1 atau 2:** {product_info_detail[6]}")
+                    st.write(f"**Lokasi:** {product_info_detail[4]}")
+                    st.write(f"**Nama Pengepul 1 atau 2:** {product_info_detail[6]}")
+                    st.write(f"**Tanggal Penjualan ke pengepul 1 atau 2:** {product_info_detail[5]}")
 
                     # Menampilkan gambar jika ada
                     if product_info_detail[8]:
