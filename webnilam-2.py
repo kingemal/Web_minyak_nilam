b5afbadc6793ee2ba76adc06e2fa2eaaa72ec4ac
Kemalfarsa007 <113975189+kingemal@users.noreply.github.com>
2025-04-04 23:56:18 +0700

    Update webnilam-2.py

--- webnilam-2.py
+++ webnilam-2.py
@@ -431,7 +431,7 @@ def penelusuran(Id_Penyulingan=None):
             st.header("Alamat Petani Nilam Aceh")
 
             # Koordinat lokasi Pemasok
-            lokasi = [5.5598861, 95.31965]  # Lokasi aceh jaya
+            lokasi = [3.323170, 97.176502]  # Lokasi aceh jaya
 
             # Kunci API Mapbox (Ganti dengan kunci API Anda sendiri)
             pdk.settings.api_key = "YOUR_MAPBOX_API_KEY"
